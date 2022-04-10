1c3df7ec9d897aa58dec5ce8d34d7493ca990560
Swearfy <swearfy@gmail.com>
2022-04-10 02:23:10 +0100

    populate text areas from listview with format

--- Module_Chooser/src/view/OverviewSelectionPane.java
+++ Module_Chooser/src/view/OverviewSelectionPane.java
@@ -7,44 +7,45 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class OverviewSelectionPane extends VBox{
+public class OverviewSelectionPane extends VBox {
 
-    public OverviewSelectionPane(){
-        //Button
-        Button btn_save = new Button("Save");
+    Button btn_save;
 
-        //Text Area
-        TextArea profile = new TextArea ();
+    TextArea profile, txt_slcdModules, txt_resvdModules;
+    String d;
+
+    public OverviewSelectionPane() {
+        // Button
+        btn_save = new Button("Save");
+
+        // Text Area
+        profile = new TextArea();
         profile.setPrefSize(5000, 5000);
         profile.setEditable(false);
-        profile.setMouseTransparent(true);
         profile.setFocusTraversable(false);
         profile.setPromptText("Profile will display here");
 
-        TextArea txt_slcdModules = new TextArea ();
+        txt_slcdModules = new TextArea();
         txt_slcdModules.setPrefSize(5000, 5000);
         txt_slcdModules.setEditable(false);
-        txt_slcdModules.setMouseTransparent(true);
         txt_slcdModules.setFocusTraversable(false);
         txt_slcdModules.setPromptText("Selected Module will show here");
+        txt_slcdModules.setWrapText(true);
 
-        TextArea txt_resvdModules = new TextArea ();
+        txt_resvdModules = new TextArea();
         txt_resvdModules.setPrefSize(5000, 5000);
         txt_resvdModules.setEditable(false);
-        txt_resvdModules.setMouseTransparent(true);
         txt_resvdModules.setFocusTraversable(false);
         txt_resvdModules.setPromptText("Reserved Module will show here");
-
-
-
+        txt_resvdModules.setWrapText(true);
 
         HBox hbox = new HBox();
         hbox.setSpacing(10);
-        hbox.getChildren().addAll(txt_slcdModules,txt_resvdModules);
+        hbox.getChildren().addAll(txt_slcdModules, txt_resvdModules);
 
-        this.getChildren().addAll(profile,hbox,btn_save);
+        this.getChildren().addAll(profile, hbox, btn_save);
 
-        //Styling
+        // Styling
         hbox.prefHeightProperty().bind(this.heightProperty());
         hbox.prefWidthProperty().bind(this.widthProperty());
         profile.prefHeightProperty().bind(hbox.heightProperty());
@@ -55,7 +56,17 @@ public class OverviewSelectionPane extends VBox{
         this.setMargin(profile, new Insets(10, 10, 10, 10));
         this.setMargin(btn_save, new Insets(10, 10, 10, 10));
 
-
     }
-    
+
+    public void setProfile(String s) {
+        profile.setText(s);
+    }
+
+    public void setSelectModules(String s) {
+        txt_slcdModules.setText(s);
+    }
+
+    public void setReserveModule(String s) {
+        txt_resvdModules.setText(s);
+    }
 }
